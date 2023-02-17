5386e05cee35cb16eed59dab37c22a8321d45672
BiancaPeter <botabiancateodora@gmail.com>
2023-02-17 16:33:44 +0200

    modified RoomReservation

--- src/main/java/com/spring/booking/model/RoomReservation.java
+++ src/main/java/com/spring/booking/model/RoomReservation.java
@@ -1,8 +1,6 @@
 package com.spring.booking.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
