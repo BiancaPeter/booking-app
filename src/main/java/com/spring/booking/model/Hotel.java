0d39770fd1e170763fcdc273f7ed133fbcce2221
BiancaPeter <botabiancateodora@gmail.com>
2023-03-23 10:34:31 +0200

    update hotel model

--- src/main/java/com/spring/booking/model/Hotel.java
+++ src/main/java/com/spring/booking/model/Hotel.java
@@ -1,6 +1,7 @@
 package com.spring.booking.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -10,13 +11,16 @@ import java.util.List;
 public class Hotel {
     @Id
     @GeneratedValue
+    @ApiModelProperty(hidden = true)
     private Long id;
 
     @Column
+    @ApiModelProperty(notes = "Hotel name", example = "Continental", required = true)
     private String name;
 
     @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL)
     @JsonManagedReference(value = "hotel-room")
+    @ApiModelProperty(hidden = true)
     private List<Room> roomList;
 
     public Hotel() {
